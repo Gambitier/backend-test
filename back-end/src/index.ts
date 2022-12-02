85aff5ac3a4e18926f6669fdb6d55cb9cb9a038e
Akash Jadhav <csegambitier@hotmail.com>
2022-12-02 15:58:54 +0530

    add exception handler

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -11,6 +11,7 @@ createConnection()
   .then(async (connection) => {
     // create express app
     const app = express()
+
     app.use(cors())
     app.use(bodyParser.json())
 
@@ -19,129 +20,144 @@ createConnection()
       ;(app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
         const result = new (route.controller as any)()[route.action](req, res, next)
         if (result instanceof Promise) {
-          result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined))
+          result.then((result) => (result !== null && result !== undefined ? res.send(result) : res.send(null)))
         } else if (result !== null && result !== undefined) {
           res.json(result)
         }
       })
     })
 
+    app.use(function (err, req, res, next) {
+      if (res.headersSent) {
+        return next(err)
+      }
+
+      // TODO add global exception handler
+      res.status(500)
+      res.send({
+        // message: "something went wrong",
+        error: err.message,
+      })
+    })
+
     // start express server
     app.listen(4001)
 
-    // insert 15 students
-
-    await connection.manager.find(Student).then(async (students) => {
-      console.log("We have " + students.length + " students")
-      if (students.length === 0) {
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "David",
-            last_name: "Bowie",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Robert",
-            last_name: "Plant",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "James",
-            last_name: "Bond",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Bob",
-            last_name: "Marley",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Paul",
-            last_name: "McCartney",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "George",
-            last_name: "Harrison",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Elton",
-            last_name: "John",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Simon",
-            last_name: "Joyner",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "John",
-            last_name: "Denver",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Neil",
-            last_name: "Diamond",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Donna",
-            last_name: "Summer",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Aretha",
-            last_name: "Franklin",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Diana",
-            last_name: "Ross",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Kate",
-            last_name: "Bush",
-            photo_url: "",
-          })
-        )
-        await connection.manager.save(
-          connection.manager.create(Student, {
-            first_name: "Boz",
-            last_name: "Scaggs",
-            photo_url: "",
-          })
-        )
-      }
-    })
+    await seedData(connection)
 
     console.log("Express server has started on port 4001. Open http://localhost:4001/student/get-all to see results")
   })
   .catch((error) => console.log(error))
+
+async function seedData(connection) {
+  await connection.manager.find(Student).then(async (students) => {
+    console.log("We have " + students.length + " students")
+    if (students.length === 0) {
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "David",
+          last_name: "Bowie",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Robert",
+          last_name: "Plant",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "James",
+          last_name: "Bond",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Bob",
+          last_name: "Marley",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Paul",
+          last_name: "McCartney",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "George",
+          last_name: "Harrison",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Elton",
+          last_name: "John",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Simon",
+          last_name: "Joyner",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "John",
+          last_name: "Denver",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Neil",
+          last_name: "Diamond",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Donna",
+          last_name: "Summer",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Aretha",
+          last_name: "Franklin",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Diana",
+          last_name: "Ross",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Kate",
+          last_name: "Bush",
+          photo_url: "",
+        })
+      )
+      await connection.manager.save(
+        connection.manager.create(Student, {
+          first_name: "Boz",
+          last_name: "Scaggs",
+          photo_url: "",
+        })
+      )
+    }
+  })
+}
